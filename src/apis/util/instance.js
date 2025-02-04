7819687233c6e17edeaafc03bd379383a8087439
hyunju1219 <kk020123@naver.com>
2025-02-05 02:03:16 +0900

    Feat: API 요청 주소를 EC2 탄력적 IP로 변경

--- src/apis/util/instance.js
+++ src/apis/util/instance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const liveServerAddress = "https://scram.site:8080";
+const liveServerAddress = "http://15.165.96.196";
 const localServerAddress = "http://localhost:8080";
-export const IMAGE_ADDRESS = localServerAddress + "/images/";
+export const IMAGE_ADDRESS = "https://mn-image-s3-bucket.s3.ap-northeast-2.amazonaws.com/";
 
 export const instance = axios.create({
-    baseURL: localServerAddress,
+    baseURL: liveServerAddress,
     headers: {
         Authorization: localStorage.getItem("accessToken")
     }
